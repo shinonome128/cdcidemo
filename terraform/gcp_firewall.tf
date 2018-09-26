b1e76eeb5f22ae9f6075a6cf17947a2353f8fccf
shinonome128 <shinonome128@gmail.com>
2018-09-26 20:09:39 +0900

    Change firewall rule

--- terraform/gcp_firewall.tf
+++ terraform/gcp_firewall.tf
@@ -1,1 +1,17 @@
-# gcp_firewall.tf 
+# gcp_firewall.tf
+
+resource "google_compute_firewall" "development" {
+  name    = "development"
+  network = "${google_compute_network.gcp-2016-advent-calendar.name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "443"]
+  }
+
+  target_tags = ["${google_compute_instance.development.tags}"]
+}

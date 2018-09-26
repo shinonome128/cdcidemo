124e68c1e4ecc3b0920d2a16d91b6af7d2e757de
shinonome128 <shinonome128@gmail.com>
2018-09-26 23:06:03 +0900

    Change path

--- terraform/gcp_provider.tf
+++ terraform/gcp_provider.tf
@@ -2,7 +2,8 @@
 // Configure the Google Cloud provider
 provider "google" {
   # credentials = "${file("../cicd-demo-707b32bf1a7f.json")}"
-  credentials = "${file("C:\Users\shino\doc\cdcidemo\cicd-demo-707b32bf1a7f.json")}"
+  # credentials = "${file("C:\Users\shino\doc\cdcidemo\cicd-demo-707b32bf1a7f.json")}"
+  credentials = "${file("C:/Users/shino/doc/cdcidemo/cicd-demo-707b32bf1a7f.json")}"
   project     = "cicd-demo"
   region      = "us-east1"
 }

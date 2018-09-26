a03b29ed168a9cc6e69d3e3668d8c8a407a739cd
shinonome128 <shinonome128@gmail.com>
2018-09-26 20:04:14 +0900

    Add Coode

--- terraform/gcp_network.tf
+++ terraform/gcp_network.tf
@@ -1,1 +1,12 @@
 # gcp_network.tf 
+
+resource "google_compute_network" "gcp-2016-advent-calendar" {
+  name = "gcp-2016-advent-calendar"
+}
+resource "google_compute_subnetwork" "development" {
+  name          = "development"
+  ip_cidr_range = "10.30.0.0/16"
+  network       = "${google_compute_network.gcp-2016-advent-calendar.name}"
+  description   = "development"
+  region        = "us-east1"
+}

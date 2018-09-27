0572110ad21b1b2f3f35e1244f36aed272cbad61
shinonome128 <shinonome128@gmail.com>
2018-09-28 04:26:13 +0900

    Change network name and region

--- terraform/gcp_network.tf
+++ terraform/gcp_network.tf
@@ -1,12 +1,16 @@
 # gcp_network.tf 
 
-resource "google_compute_network" "gcp-2016-advent-calendar" {
-  name = "gcp-2016-advent-calendar"
+## resource "google_compute_network" "gcp-2016-advent-calendar" {
+##   name = "gcp-2016-advent-calendar"
+resource "google_compute_network" "cicddemo" {
+  name = "cicddemo"
 }
 resource "google_compute_subnetwork" "development" {
   name          = "development"
   ip_cidr_range = "10.30.0.0/16"
-  network       = "${google_compute_network.gcp-2016-advent-calendar.name}"
+  # network       = "${google_compute_network.gcp-2016-advent-calendar.name}"
+  network       = "${google_compute_network.cicddemo.name}"
   description   = "development"
-  region        = "us-east1"
+  # region        = "us-east1"
+  region        = "asia-northeast1"
 }

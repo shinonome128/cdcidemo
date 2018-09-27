565931658cc43c9310c79e026eb1b44b88306638
shinonome128 <shinonome128@gmail.com>
2018-09-27 19:25:17 +0900

    Change project id

--- terraform/gcp_provider.tf
+++ terraform/gcp_provider.tf
@@ -4,6 +4,7 @@ provider "google" {
   # credentials = "${file("../cicd-demo-707b32bf1a7f.json")}"
   # credentials = "${file("C:\Users\shino\doc\cdcidemo\cicd-demo-707b32bf1a7f.json")}"
   credentials = "${file("C:/Users/shino/doc/cdcidemo/cicd-demo-707b32bf1a7f.json")}"
-  project     = "cicd-demo"
+  # project     = "cicd-demo"
+  project     = "cicd-demo-215605"
   region      = "us-east1"
 }

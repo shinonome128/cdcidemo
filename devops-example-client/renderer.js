a63c1df96b07d1fdf9729348dfebb748629a7381
shinonome128 <shinonome128@gmail.com>
2018-10-01 20:01:07 +0900

    Add server address

--- devops-example-client/renderer.js
+++ devops-example-client/renderer.js
@@ -2,10 +2,37 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-document.querySelector('#btn').addEventListener('click', getData);
+// document.querySelector('#btn').addEventListener('click', getData);
+// 
+// function getData() {
+//     // TODO implements
+//     document.querySelector('#result').innerHTML = "First release";
+// }
 
-function getData() {
-    // TODO implements
-    document.querySelector('#result').innerHTML = "First release";
-}
-
+// const hostname = '<サーバのグローバルIPアドレス>'  
+const hostname = '<35.221.101.2>'  
+const remote = require('electron').remote  
+  
+document.querySelector('#btn').addEventListener('click', getData);  
+  
+function getData() {  
+    const net = remote.net;  
+    const request = net.request({  
+        method: 'GET',  
+        protocol: 'http:',  
+        hostname: hostname,  
+        port: 80,  
+        path: '/example.php'  
+    })  
+  
+    request.on('response', (response) => {  
+        document.querySelector('#result').innerHTML  = ""  
+        response.on('data', (chunk) => {  
+            document.querySelector('#result').innerHTML += chunk  
+        })  
+    })  
+    request.on('error', (err) => {  
+        document.querySelector('#result').innerHTML = `ERROR: ${JSON.stringify(err)}`  
+    })  
+    request.end()  
+}  

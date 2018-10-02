cc6c10cf555278af53dfd5634d7d3651067848c9
shinonome128 <shinonome128@gmail.com>
2018-10-02 18:04:50 +0900

    Add global ip

--- terraform/gcp_firewall.tf
+++ terraform/gcp_firewall.tf
@@ -17,7 +17,8 @@ resource "google_compute_firewall" "development" {
   # source_ranges = ["0.0.0.0/0"]
   # source_ranges = ["116.220.197.54/32"]
   # source_ranges = ["116.220.197.54/32", "173.194.92.0/23"]
-  source_ranges = ["116.220.197.54/32", "173.194.92.0/23", "49.239.66.40/32"]
+  # source_ranges = ["116.220.197.54/32", "173.194.92.0/23", "49.239.66.40/32"]
+  source_ranges = ["116.220.197.54/32", "173.194.92.0/23", "49.239.66.40/32", "126.112.246.62/32"]
 
   target_tags = ["${google_compute_instance.development.tags}"]
 }

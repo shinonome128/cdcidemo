d08df4e2ebe929a0030716656f5ba545452f68e7
shinonome128 <shinonome128@gmail.com>
2018-10-04 13:03:33 +0900

    Change start shell

--- terraform/gcp_instances.tf
+++ terraform/gcp_instances.tf
@@ -31,20 +31,38 @@ resource "google_compute_instance" "development" {
   scratch_disk {
   }
 
-/* temp invalid
   // app install
   metadata_startup_script = <<EOT
 #!/bin/sh 
 
-yum update -y 
-yum install -y httpd php 
-systemctl enable httpd.service 
-systemctl start httpd.service 
-firewall-cmd --add-service=http --permanent 
-firewall-cmd --reload 
-yum install -y git  
+# yum update -y 
+# yum install -y httpd php 
+# systemctl enable httpd.service 
+# systemctl start httpd.service 
+# firewall-cmd --add-service=http --permanent 
+# firewall-cmd --reload 
+# yum install -y git  
+
+# install base
+sudo apt-get update -y  
+sudo apt-get install -y apache2  
+sudo apt-get install -y php  
+sudo systemctl apache2 restart  
+sudo apt-get install -y git  
+
+# install server-app
+git clone https://github.com/shinonome128/devops-example-server.git  
+cd devops-example-server  
+sudo cp example.php /var/www/html/  
+
+# install travics cli
+sudo apt-get install -y ruby ruby-dev  
+sudo apt-get install -y gcc  
+sudo apt-get install -y libffi-dev  
+sudo apt-get install -y make  
+sudo gem install travis  
+
 EOT
-*/
 
   network_interface {
     access_config {
